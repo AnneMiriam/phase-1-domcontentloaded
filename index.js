7f0a041b86d8de120380c916e90567ff3afc4848
Anne Hastings <anne.miriam@gmail.com>
2023-10-31 15:49:19 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+const changeText = document.getElementById('text');
+
+document.addEventListener('DOMContentLoaded', function() {
+    changeText.textContent = 'This is really cool!'
+});
